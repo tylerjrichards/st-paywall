93c15f0f9bed67dbb8c4f3761fd214707ce42c58
codingeologist <codingeologist@gmail.com>
2025-06-12 19:19:12 +0100

    replace st.experimental_user with st.user

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ st.set_page_config(layout="wide")
 st.title("🎈 Tyler's Subscription app POC 🎈")
 st.balloons()
 
-if not st.experimental_user.is_logged_in:
+if not st.user.is_logged_in:
     if st.button("Log in using Streamlit's native authentication"):
         st.login()
 
 else:
     if st.button("Log out"):
         st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")
+    st.write(f"Hello, {st.user.name}!")
     st.write('Now we can use this to check if the user is a subscriber!')
     add_auth(required=True, show_redirect_button=True, use_sidebar=False)
 
     st.write("Congrats, you are subscribed!")
-    st.write("the email of the user is " + str(st.experimental_user.email))
+    st.write("the email of the user is " + str(st.user.email))
